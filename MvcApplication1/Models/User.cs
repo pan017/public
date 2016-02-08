ecc12cee6feb4cd55ac7054fb534beec9fad78f3
pan017 <pan017@yandex.by>
2016-02-08 13:55:43 +0300

    kgf,vgucft,ctgydc c dc
    Signed-off-by: pan017 <pan017@yandex.by>
    ервый модуль тренинга по ASP.NET MVC 3 знакомит с материалом, который будет в дальнейшем представлен в тренинги, рассказывает про необходимое программное обеспечение и проводит краткий обзор концепции MVC приложений. Тренинг предназначен для начинающих разработчиков, изучающих ASP.NET MVC 3. Для прохождения тренинга необходимо знанени основ языка C# и желательно знание

---
+++ MvcApplication1/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
---
+++ MvcApplication1/Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+        { }
+        public DbSet<User> Users { get; set; }
+    }
+}
